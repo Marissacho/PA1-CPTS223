f256db8b2c1807daa572e99e57c2291c05bd8436
marissa <109318586+Marissacho@users.noreply.github.com>
2024-09-23 00:00:30 -0700

    Update linkedlist.hpp

--- linkedlist.hpp
+++ linkedlist.hpp
@@ -1,1 +1,33 @@
+#ifndef
+#include <fstream>
+#include <iostream>
 
+using namespace std;
+
+template<typename t, typename d>
+
+class Node{
+private:
+t command;
+d description;
+Node*next;
+};
+
+template<typename t, typename d>
+class Linked_List{
+private:
+Node<t,d>*head;
+size_1 node;
+
+public:
+void playGame();
+void removeCommand(t&command);
+void addCommand(const t&command, d& description);
+bool commandfinder(const t&s_Command);
+void outfile(const string& file);
+int removeNode();
+void insertAtFront(const t& command, const d& description);
+~Linked_List();
+Linked_List();
+};
+#endif
